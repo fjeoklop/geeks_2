1fff5ec973bab7f453e66e76a88d22b1661ff9f0
fjeoklop <127516587+fjeoklop@users.noreply.github.com>
2023-04-10 20:01:33 +0600

    сделал дз 2

---
+++ dz2_bek_25-01/main.js
@@ -0,0 +1,46 @@
+//ex 1
+// let arr = [10, 20, 30, 50, 235, 3000];
+// console.log(arr.filter(num => ['1','2','5'].includes(num.toString()[0])));
+//
+// ex2
+//
+// for ( var i = 20; i >= 0; i--) {
+//     console.log(i)
+// }
+//
+// ex3
+//
+// var obj = {
+//     red: 'красный',
+//     yellow:'желтый',
+//     green:'зеленый'
+// }
+// switch (prompt('введите цвета')){
+//     case obj.red:
+//         alert('стоп')
+//         break
+//     case obj.yellow:
+//         alert('приготовится')
+//         break
+//     case obj.green:
+//         alert('вперед')
+//         break
+//     default:
+//         alert('введите цвета')
+// }
+//
+// ex4
+//
+// var arr = []
+// arr [0] = 0
+// arr [1] = 1
+// arr [2] = 2
+// arr [3] = 3
+// arr [4] = 4
+// arr [5] = 5
+// arr [6] = 6
+// arr [7] = 7
+// arr [8] = 8
+// arr [9] = 9
+// arr [10] = 10
+// console.log(arr)
